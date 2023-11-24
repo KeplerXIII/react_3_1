065398271b3b7f1e2e7c140c6937819c7e280518
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-11-24 11:01:02 +0300

    upd

--- films_grade/src/App.tsx
+++ films_grade/src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  
+  function Stars ({ count }: {count: number}): JSX.Element[] {
+
+    const starJSX = <ul className="card-body-stars u-clearfix">
+                      <li>
+                          <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
+                              <path d="M0 0h18v18H0z" fill="none"/>
+                          </svg>
+                      </li>
+                    </ul>
+
+    return Array.from({ length: count }, () => starJSX);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='film-card'>
+      <Stars count={3}/>
+    </div>
   )
 }
 
